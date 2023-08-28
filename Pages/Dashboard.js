3304365fd34b98d44c07ec7f43a8ddb6e583fb3b
Rahul <rahulkr11886@gmail.com>
2023-08-28 21:23:35 +0530

    some change

--- Pages/Dashboard.js
+++ Pages/Dashboard.js
@@ -9,19 +9,30 @@ const height = Dimensions.get('window').height;
 
 export default function Dashboard({ navigation }) {
     const [name, setName] = useState('');
-    const [phone, setPhone] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     return (
         <>
             <LinearGradient
                 colors={['#2E2B69', 'orange', 'red', '#2A12CC']}
                 style={styles.linearGradient}>
-                <View style={{ flexDirection:'row', justifyContent:'space-between' }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                     <MaterialCommunityIcons name="menu" size={30} color="white" style={{ marginTop: width * 0.14, marginLeft: 10 }} />
-                    <MaterialCommunityIcons name="shopping-outline" size={30} color="white" style={{ marginTop: width * 0.14, marginRight:10}} />
+                    <MaterialCommunityIcons name="shopping-outline" size={30} color="white" style={{ marginTop: width * 0.14, marginRight: 10 }} />
                 </View>
                 <Text style={styles.title}>WELCOME TO FOOD DELIVERY</Text>
+                <View style={styles.inputView}>
+                <MaterialCommunityIcons name="search-web" size={34}  style={{position:"absolute"}}  />
+                    <TextInput
+                        style={styles.inputText}
+                        placeholder="     Search"
+                        autoComplete='off'
+                        onChangeText={text => setName(text)} />
+                </View>
+                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+                <Text style={{fontSize:15, fontWeight:'bold', marginLeft:15, marginTop:20}}>
+                    All Categories
+                </Text>
+                <MaterialCommunityIcons name="chevron-double-right" size={34} style={{marginTop:15, marginRight:15}} />
+                </View>
             </LinearGradient>
         </>
     );
@@ -37,12 +48,26 @@ const styles = StyleSheet.create({
     linearGradient: {
         flex: 1
     },
-    title:{
-        fontWeight:'bold',
-        fontSize:width*0.05,
-        color:'violet',
+    inputView: {
+        width: "90%",
+        backgroundColor: "#F6F6F6",
+        height: 50,
+        borderRadius:8,
         marginTop:25,
-        textAlign:'center',
+        justifyContent: "center",
+        alignSelf:'center',
+        padding: 20,
+    },
+    inputText: {
+        height: 50,
+        color: "black",
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: width * 0.05,
+        color: 'violet',
+        marginTop: 25,
+        textAlign: 'center',
     }
 
 });
